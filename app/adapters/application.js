d584a395b1d9b136af9086f872c73ea012ac0fc8
ngel <angel.arancibia@icloud.com>
2016-04-13 11:11:51 -0300

    integrate with rails api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,19 @@
+import ActiveModelAdapter from 'active-model-adapter';
+
+export default ActiveModelAdapter.extend({
+  host: 'http://api.hiccup.dev'
+});
+
+/*import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend({
+  host: 'http://api.hiccup.dev'
+});
+*/
 // app/adapters/application.js
-import LSAdapter from 'ember-localstorage-adapter';
+/*import LSAdapter from 'ember-localstorage-adapter';
 
 export default LSAdapter.extend({
   namespace: 'hiccup'
 });
+*/
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,10 @@
+/*
 // app/serializers/application.js
 import { LSSerializer } from 'ember-localstorage-adapter';
 
 export default LSSerializer.extend();
+*/
+
+import { ActiveModelSerializer } from 'active-model-adapter';
+
+export default ActiveModelSerializer.extend();
